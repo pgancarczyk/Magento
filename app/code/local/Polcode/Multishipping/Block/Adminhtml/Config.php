5322e4d863c4c33d81a8add1896491c79d3023bd
Paweł Gancarczyk <gancarczyk@konf.ml>
2016-09-02 15:40:39 +0200

    js class init

--- app/code/local/Polcode/Multishipping/Block/Adminhtml/Config.php
+++ app/code/local/Polcode/Multishipping/Block/Adminhtml/Config.php
@@ -1,24 +1,71 @@
 <?php
 class Polcode_Multishipping_Block_Adminhtml_Config extends Mage_Adminhtml_Block_Template {
    
-    protected $_collection;
+    protected $_configTable;
     
-    public function __construct() {
-        $_collection = Mage::getModel('multishipping/multishipping')->getCollection();
+    public function __construct()
+    {
+        $collection = Mage::getModel('multishipping/multishipping')->getCollection();
+        foreach ($collection as $row)
+        {
+             $day = $row->getDay();
+             $hour = $row->getHour();
+             $price = $row->getPrice();
+             $limit = $row->getLimit();
+             $this->_configTable[$day][$hour]['price'] = $price;
+             $this->_configTable[$day][$hour]['limit'] = $limit;
+        }
         parent::__construct();
     }
     
-    public function getPrice($day, $hour) {
-        $collection = $this->collection;
-        return Mage::getModel('multishipping/multishipping')->getCollection()
-                ->addFieldToFilter('day', array('eq' => 1))
-                ->addFieldToFilter('hour', array('eq' => 12))
-                ->load();
-//        return $row;
+    public function getPriceHtml($day, $hour)
+    {
+        $value = '';
+        if (isset($this->_configTable[$day][$hour]['price']))
+        {
+            $value = $this->_configTable[$day][$hour]['price'];
+        }
+        return "<input class=price data-multishipping-day=". $day ." data-multishipping-hour=". $hour ." type=text value=". $value .">";
     }
     
-    public function getInputClass($day, $hour) {
-        return 'enabled';
+    public function getLimitHtml($day, $hour)
+    {
+        $value = '';
+        if (isset($this->_configTable[$day][$hour]['limit']))
+        {
+            $value = $this->_configTable[$day][$hour]['limit'];
+        }
+        return "<input class=limit data-multishipping-day=". $day ." data-multishipping-hour=". $hour ." type=text value=". $value .">";
     }
     
-}
+    public function getEnabledHtml($day, $hour)
+    {
+        $selected = $this->getSelected($day, $hour);
+        return "<select data-multishipping-day=". $day ." data-multishipping-hour=". $hour ."><option value=enabled". $selected['enabled'] .">". $this->__("Enabled") ."</option><option value=disabled". $selected['disabled'] .">". $this->__("Disabled") ."</option><option ". $selected['default'] .">". $this->__("Default") ."</option></select>";
+    }
+    
+    protected function getSelected($day, $hour) 
+    {
+        $selected['enabled'] = '';
+        $selected['disabled'] = '';
+        $selected['default'] = '';        
+        if (isset($this->_configTable[$day][$hour]['enabled']))
+        {
+            switch ($this->_configTable[$day][$hour]['enabled'])
+            {
+                case 0:
+                    $selected['default'] = ' selected';
+                case 1:
+                    $selected['enabled'] = ' selected';
+                case 2:
+                    $selected['disabled'] = ' selected';
+            }
+        }
+        else
+        {
+            $selected['default'] = ' selected';
+        }
+        return $selected;
+    }
+    
+}                                                
--- js/multishipping/multishipping.js
+++ js/multishipping/multishipping.js
@@ -1,1 +1,42 @@
-console.log('dupa');
+var MultishippingConfig = Class.create();
+
+MultishippingConfig.prototype = {
+    
+    initialize: function () {
+        document.observe('change', this.identify.bind(this));
+        console.log(this);
+    },
+    
+    identify: function (event) {
+        var element = event.target;
+        if (element.hasAttribute('data-multishipping-day') && element.hasAttribute('data-multishipping-hour'))
+        {
+            var entity = Class.create();
+            entity.day = element.getAttribute('data-multishipping-day');
+            entity.hour = element.getAttribute('data-multishipping-hour');
+            entity.value = element.value;
+            if (element.nodeName === 'SELECT')
+            {
+                entity.field = 'enabled';
+            }
+            else if (element.hasClassName('price'))
+            {
+                entity.field = 'price';
+            }
+            else
+            {
+                entity.field = 'limit';
+            }
+            this.update(entity);
+        }
+    },
+    
+    update: function (entity) {
+        console.log(entity.day);
+    }
+    
+};
+
+document.observe("dom:loaded", function() {
+    new MultishippingConfig();
+});
